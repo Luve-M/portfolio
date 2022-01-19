1cda4c63dd91e673ebb59243b3a6ebd0be33d7b3
lmashru <lmashru@nouveau-labs.com>
2022-01-19 20:10:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,220 @@
+import "primereact/resources/themes/lara-light-indigo/theme.css"; 
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";        
+import 'primeflex/primeflex.css';
 import logo from './logo.svg';
 import './App.css';
+import Texts from "./Texts";
+
+import { useState } from "react";
+import { Button } from 'primereact/button';
+import { Panel } from 'primereact/panel';
 
 function App() {
+  const [aboutmeButton, setAboutMeButton] = useState(true)
+  const [workExpButton, setWorkExpButton] = useState(false)
+  const [Achievements, setAchievements] = useState(false)
+  const [Projects, setProjects] = useState(false)
+
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="p-grid">
+          <div className="p-col-2">
+            <img src={logo} className="App-logo flex items-center justify-center" alt="logo" />
+          </div>
+          <div className="p-col-10 items-center p-mt-5">
+          <div>Name: Shailja Thakur</div>
+          <div>Number: +91 80758 91171</div>
+          <div>Email: shailja@iiitkalyani.ac.in</div>
+          <div>To traverse down the world, carving a niche with all the acquired, accomplished, conquering skills.
+</div>
+        </div>
+        </div>
       </header>
+      <div className="p-grid">
+        <div className="p-col-2 background-panel">
+          <div className="flex items-center justify-center">
+            <Button 
+              label="About" 
+              className="p-button-secondary w-full p-m-1"
+              onClick={()=>{
+                setAboutMeButton(true)
+                setWorkExpButton(false)
+                setAchievements(false)
+                setProjects(false)
+              }}
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <Button 
+              label="Work Experience" 
+              className="p-button-secondary w-full p-m-1"
+              onClick={()=>{
+                setAboutMeButton(false)
+                setWorkExpButton(true)
+                setAchievements(false)
+                setProjects(false)
+              }}
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <Button 
+              label="Achievements" 
+              className="p-button-secondary w-full p-m-1"
+              onClick={()=>{
+                setAboutMeButton(false)
+                setWorkExpButton(false)
+                setAchievements(true)
+                setProjects(false)
+              }}
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <Button 
+              label="Projects" 
+              className="p-button-secondary w-full p-m-1"
+              onClick={()=>{
+                setAboutMeButton(false)
+                setWorkExpButton(false)
+                setAchievements(false)
+                setProjects(true)
+              }}
+            />
+          </div>
+        </div>
+        <div className="p-col-10">
+        {aboutmeButton && <Panel header = "About Me" className={`p-mt-1 p-mr-1`}>
+            <p>{Texts.aboutMe.line1}</p>
+            <p>{Texts.aboutMe.line2}</p>
+            <p><ul>
+            <li>
+                {Texts.aboutMe.line3}
+              </li>
+              <li>
+                {Texts.aboutMe.line4}
+              </li>
+            </ul></p>
+            <p>{Texts.aboutMe.line5}</p>
+            <p><ol>
+              <li>{Texts.aboutMe.line6}</li>
+              <li>{Texts.aboutMe.line7}</li>
+            </ol></p>
+            <p>{Texts.aboutMe.line8}</p>
+            <p>{Texts.aboutMe.line9}</p>
+        </Panel>}
+        {workExpButton && <Panel header="Work Experience" className={`p-mt-1 p-mr-1`}>
+          <div className="p-grid">
+            <div className="p-col-10 boldText">Intangles Lab, Applied Machine Learning Engineer</div>
+            <div className="p-col-2">11/2019 - 10/2021</div>
+          </div><hr/>
+          <ul>
+            <li>
+            Generated Fleet Performance Insight for OEMs, the process
+            generates insights for requested duration in regards to - Fuel, Distance, Driven Hours, Idling, Hardbraking, Overspeeding, Mileage etc in the fleet. Impact was
+            improvement in daily driver duty.
+            </li>
+            <li>
+            Generated Driver Performance Model using TOPSIS. The
+            algorithm generates performance insights for each driver
+            duty for a particular route. The model helped customers in
+            keeping a track of good and bad driver duty and rewarding
+            and warning accordingly.
+            </li>
+            <li>
+            Improvised algorithm for live tracking Adblue level
+            mechanism in vehicles. The accuracy went from 75% to 99%. OEM customers were then able to keep a track of thefts, refills, b-point in real time.
+            </li>
+          </ul>
+
+          <div className="p-grid">
+            <div className="p-col-10 boldText">Shipsy, Data Scientist Intern</div>
+            <div className="p-col-2">08/2019 - 10/2019</div>
+          </div><hr/>
+          <ul>
+            <li>
+            Devised an algorithm that increased accuracy score from
+            50% to 82% for one of the strategies to geocode logistic
+            delivery addresses. It was further used by delivery agents
+            across the Hub - Gurgaon.
+            </li>
+          </ul>
+
+          <div className="p-grid">
+            <div className="p-col-10 boldText">Tech-Tailor Solutions Pvt. Ltd., Thermal Image Processing Intern</div>
+            <div className="p-col-2">12/2017 - 01/2018</div>
+          </div><hr/>
+          <ul>
+            <li>
+            Generated Automated Measurement Tool for Human Body
+            measurement using the concept of Thermal Imaging with
+            the help of MATLAB/Octave, accuracy was around 90% for 6
+            measurements. The project was deployed for final use-case
+            </li>
+          </ul>
+        </Panel>}
+        {Achievements && <Panel header="Achievements" className={`p-mt-1 p-mr-1`}>
+        <div className="p-grid">
+            <div className="p-col-10 boldText">N/Core Ideathon Winner</div>
+            <div className="p-col-2">06/2018</div>
+          </div><hr/>
+          <ul>
+            <li>
+            Project Awaaz - One Lakh Seed Grant. From a total of 946 teams, we were among the top 10 and as well as the Winner. We received an offer of incubation as well as prize money of 1 Lakh.
+            </li>
+          </ul>
+          
+          <div className="p-grid">
+            <div className="p-col-10 boldText">Smart India Hackathon</div>
+            <div className="p-col-2">2019</div>
+          </div><hr/>
+          <ul>
+            <li>
+            From a total of 1000+ ideas on the problem statement of 'PERSON
+            TRACKING SYSTEM' given by HCL, our Team was chosen among the top
+            5, we also received an offer to come onsite and implement our proposed
+            solution.
+            </li>
+          </ul>
+        </Panel>}
+        {Projects && <Panel header="Projects" className={`p-mt-1 p-mr-1`}>
+        <div className="p-grid">
+            <div className="p-col-10 boldText">Intelligent Document Processing System</div>
+          </div><hr/>
+          <ul>
+            <li>
+            The goal was to build a multi-classifier to classify a set of 35k
+            documents. Generated TF-IDF score to vectorize the text.
+            </li>
+            <li>Logistic Regression model to build a multi-classifier. With an
+            accuracy score of 98%</li>
+          </ul>
+
+          <div className="p-grid">
+            <div className="p-col-10 boldText">Person Tracking and Re-identification</div>
+          </div><hr/>
+          <ul>
+            <li>
+            The goal of the project was to uniquely identify a person in a video frame. 
+            </li>
+            <li>Used YOLO algorithm to detect person. Siamese Network to reidentify and track the person through unique features from the body.</li>
+            <li>Used MTCNN to extract face. Facenet model to re-identify through unique features from the face</li>
+          </ul>
+
+          <div className="p-grid">
+            <div className="p-col-10 boldText">Ashrae Great Energy Predictor</div>
+          </div><hr/>
+          <ul>
+            <li>
+            Assessing the value of energy efficiency improvements - predicting how much energy a building would have used.
+            </li>
+            <li>
+            Built model across four energy types based on historic usage rates and observed weather.
+            </li>
+          </ul>
+        </Panel>}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Texts.js
@@ -0,0 +1,24 @@
+const Texts = {
+ aboutMe:{ 
+  line1: `This is Shailja Thakur, 2019 Computer Science graduate from Indian Institute of Information Technology, Kalyani.`,
+
+ line2: `I am an analytics-driven person with experience in the ideation and implementation of strategies.`,
+ 
+ line3: `The last company I worked for a period of two years, Intangles Lab had a digital-twinning-based vehicle health monitoring system and performance analysis for fleet operations. As an Applied Machine Learning Engineer, my profile lied at the intersection of automotive technology, statistical modeling, data mining techniques, and computation. I managed multiple projects at a time, delivering solutions on time with improved accuracy.`,
+ 
+ line4:`I have done 2 internships earlier during my under-grad, in the field of Data Science and Computer Vision. With one being in the - digital tailoring body measurements, another being in - end-to-end logistics and supply chain operations. Thanks to the diversity of roles, I have decent exposure to tackling unique problem statements which I try to refine with every project.`,
+ 
+ line5:`Along with this I got some opportunities to display leadership qualities, some of which are -`,
+ 
+ line6:`I co-founded Literary Club - Udaan, IIIT Kalyani.
+ With initial collaborative participation from various colleges - contacted clubs of various colleges, listed down things, made plans as to how it would suit the interests of students here. Later led the team for a period of two years, with weekly fun engaging sessions - debates, creative writing, group discussions and various other collaborative sessions. Today it continues to be one of the most active clubs of IIITK.`,
+ 
+ 
+ line7:`I co-founded an NGO - Ek Kadam, (December 2019) where we take up genuine fundraising cases and plan out food distribution drives to homeless people. Till date we have taken 9 fundraising cases where we have given Rs. 1,21,923. And held 12 food distribution drives which cost Rs. 1,52,103.`,
+ 
+ line8:`I love solving problems, and have some experience in participating in Hackathons/Ideathons. To throw some light, from a total of 946 teams, an Ideathon organized by N/Core and Omidyar Networks, our team won the Ideathon with 1 lakh prize money and an offer for incubation. Also in Smart India Hackathon 2019, from a total 1000+ idea submissions we received an offer for incubation from the HCL team.`,
+ 
+ line9:`Outside of work, I pursue sports. I am a state level Badminton player, have won two town level Marathons in my school days, a Karate Orange belt. And currently learning the dance form - Jazz Funk at a professional dance school.`}
+}
+
+export default Texts;
